3f87b929bf89519500b2b55b9231027eccf7b089
Victor Johnson <vicjohnson1213@gmail.com>
2015-03-10 18:58:59 -0600

    Added Content-Type headers to text/html

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ exports.staticTemplate = function(views, opts) {
 
         res.render(url, (options.templateOpts || {}), function(err, html) {
             if (!err) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.end(html);
             } else {
                 res.render(path.join(url, 'index'), (options.templateOpts || {}), function(indexErr, indexHtml) {
                     if (!indexErr) {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
                         res.end(indexHtml);
                     } else {
                         next();
